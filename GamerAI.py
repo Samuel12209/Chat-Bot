9fcca3678f401304c2a769512f4d78d1afbf7135
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-11 06:17:47 -0400

    Device

--- GamerAI.py
+++ GamerAI.py
@@ -1,4 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision
-exit()
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+device = torch.device('cuda' if torch.cuda.is_available else 'cpu')

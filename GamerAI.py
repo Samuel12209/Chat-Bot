3599a35b6337137ecafd0aae358726b0449256b4
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-11 07:13:19 -0400

    Hyper peramiters

--- GamerAI.py
+++ GamerAI.py
@@ -5,3 +5,12 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
 device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
+
+imput_size = 784
+hidden_size = 100
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
+
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True)

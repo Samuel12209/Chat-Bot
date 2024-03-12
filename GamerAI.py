6fe633815e1d1a2185211c8ec56946f12cdb1d0d
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-12 17:19:27 -0400

    DataSet

--- GamerAI.py
+++ GamerAI.py
@@ -13,4 +13,17 @@ num_epochs = 2
 batch_size = 100
 learning_rate = 0.001
 
-train_dataset = torchvision.datasets.MNIST(root='./data', train=True)
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True,
+    transform=transforms.ToTensor(), download = True)
+
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False,
+    transform=transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size,shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size,
+    shuffle=False)
+
+example = iter(train_loader)
+samples, labels = example.next()
+print(samples.shape, labels.shape)

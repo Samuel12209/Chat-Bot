db7c818129df92dd516888e2f63d54a928624212
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-12 21:21:08 -0400

    Chatting Started w brain

---
+++ Machine Learning.py
@@ -0,0 +1,8 @@
+import json
+from difflib import get_close_matches
+brain = Users/Dogne/Code/Gamer-Ai/Brain.json
+
+def load_brain(brain: str)
+    with open(brain, 'r') as file:
+        data: dict = json.load_brain
+    return data

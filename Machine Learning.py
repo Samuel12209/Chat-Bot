fff1f52e2dd0a9dbdce250a30bde91fa0368abfd
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-12 22:11:52 -0400

    Working chatbot!

--- Machine Learning.py
+++ Machine Learning.py
@@ -1,8 +1,47 @@
 import json
 from difflib import get_close_matches
-brain = 'Brain.json'
+Brain = 'Brain.json'
 
-def load_brain(brain: str):
-    with open(brain, 'r') as file:
-        data: dict = json.load_brain
+def load_knowledge_base(file_path: str):
+    with open(file_path, 'r') as file:
+        data: dict = json.load(file)
     return data
+
+def save_knowledge_base(file_path: str, data: dict):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent = 2)
+
+
+def find_best_match(user_question:str, questions: list[str]):
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+    return matches[0] if matches else None
+
+def get_awnser_for_question(question: str, knowledge_base: dict):
+    for q in knowledge_base["questions"]:
+        if q["question"] == question:
+            return q["awnser"]
+
+def chat_bot():
+    knowledge_base: dict = load_knowledge_base(Brain)
+
+    while True:
+        user_input: str = input('You: ')
+        if user_input.lower() == 'quit':  # corrected comparison
+            break
+        
+        best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+
+        if best_match:
+            awnser: str = get_awnser_for_question(best_match, knowledge_base)
+            print(f'bot: {awnser}')
+        else:
+            print('Bot: I don\'t know the answer. Can you teach me?')
+            new_answer: str = input('Type the answer or "skip" to skip ')
+
+            if new_answer.lower() != 'skip':
+                knowledge_base["questions"].append({"question": user_input, "awnser": new_answer})
+                save_knowledge_base(Brain, knowledge_base)
+                print("Bot: Thank you! I learned a new Response!")
+
+if __name__ == '__main__':
+    chat_bot()

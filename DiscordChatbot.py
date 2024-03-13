59cf98caa4a09ccbd522b07fb68b8bc4c0bc295b
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-13 07:14:20 -0400

    Release

---
+++ DiscordChatbot.py
@@ -0,0 +1,76 @@
+import discord
+import json
+import time
+from difflib import get_close_matches
+
+Brain = 'Brain.json'
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+client = discord.Client(intents=intents)
+
+print("Hi, This is my Discord Chatbot")
+print("The responses the bot gives might not always be accurate.")
+print("-" * 10)
+time.sleep(3)
+
+
+def load_knowledge_base(file_path: str):
+    with open(file_path, 'r') as file:
+        data: dict = json.load(file)
+    return data
+
+
+def save_knowledge_base(file_path: str, data: dict):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=2)
+
+
+def find_best_match(user_question: str, questions: list[str]):
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.80)
+    return matches[0] if matches else None
+
+
+def get_answer_for_question(question: str, knowledge_base: dict):
+    for q in knowledge_base["questions"]:
+        if q["question"] == question:
+            return q["answer"]
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == 'quit':
+        await message.channel.send('Goodbye!')
+        await client.close()
+
+    user_input = message.content
+
+    knowledge_base: dict = load_knowledge_base(Brain)
+
+    best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+
+
+    if best_match:
+        answer: str = get_answer_for_question(best_match, knowledge_base)
+        await message.channel.send(f'Bot: {answer}')
+    else:
+        await message.channel.send('Bot: I don\'t know the answer. Can you teach me?(Type the awnser or "skip" to skip)')
+        response = await client.wait_for('message', check=lambda m: m.author == message.author)
+
+        if response.content.lower() != 'skip':
+            knowledge_base["questions"].append({"question": user_input, "answer": response.content})
+            save_knowledge_base(Brain, knowledge_base)
+            await message.channel.send("Bot: Thank you! I learned a new Response!")
+
+
+client.run('Your Discord Token')

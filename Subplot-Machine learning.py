5fffc2d529028d4653e451a6f4a4c6e172a635d8
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-12 21:04:26 -0400

    Finished Subplot

--- Subplot-Machine learning.py
+++ Subplot-Machine learning.py
@@ -5,12 +5,12 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
- 
-input_size = 784 
+
+input_size = 784
 hidden_size = 500 
 num_classes = 10
 num_epochs = 2
-batch_size = 100
+batch_size = 1000
 learning_rate = 0.001
 
 # MNIST dataset 
@@ -25,8 +25,8 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch
 examples = iter(test_loader)
 example_data, example_targets = next(examples)
 
-for i in range(6):
-    plt.subplot(2,3,i+1)
+for i in range(12):
+    plt.subplot(2,6,i+1)
     plt.imshow(example_data[i][0], cmap='gray')
 plt.show()
 

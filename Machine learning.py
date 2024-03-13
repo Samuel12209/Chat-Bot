06823989c5e35f2d3a903d66afa29e03807fcedf
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-12 20:53:32 -0400

    Accuracy

--- Machine learning.py
+++ Machine learning.py
@@ -4,32 +4,80 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
-device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
-
-imput_size = 784
-hidden_size = 100
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+ 
+input_size = 784 
+hidden_size = 500 
 num_classes = 10
 num_epochs = 2
-batch_size = 1000
+batch_size = 100
 learning_rate = 0.001
 
-train_dataset = torchvision.datasets.MNIST(root='./data', train=True,
-    transform=transforms.ToTensor(), download = True)
+# MNIST dataset 
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(),  download=True)
 
-test_dataset = torchvision.datasets.MNIST(root='./data', train=False,
-    transform=transforms.ToTensor())
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
 
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size,
-    shuffle=True)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size,
-    shuffle=False)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
-example = iter(train_loader)
-samples, labels = next(example)
-print(samples.shape, labels.shape)
+examples = iter(test_loader)
+example_data, example_targets = next(examples)
 
 for i in range(6):
-    plt.subplot(2, 3, i+1)
-    plt.imshow(samples[i][0], cmap='grey')
+    plt.subplot(2,3,i+1)
+    plt.imshow(example_data[i][0], cmap='gray')
 plt.show()
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.input_size = input_size
+        self.l1 = nn.Linear(input_size, hidden_size) 
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(hidden_size, num_classes)  
+    
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+
+        return out
+
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
+
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):  
+
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if (i+1) % 100 == 0:
+            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        
+        _, predictions = torch.max(outputs, 1)
+        n_samples += labels.shape[0]
+        n_correct += (predictions == labels).sum().item()
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'accuracy = {acc}')
